5c6f530842b78469f26619fcd1f1f90c95dd83b3
Guru107 <guruak107@gmail.com>
2016-10-10 18:59:23 +0530

    index

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,8 @@ module.exports = {
     },
     plugins:[
         new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+			template:'./default_index.ejs'
+		})
     ]
 }
